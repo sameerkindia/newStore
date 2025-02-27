35669026686f3d9b76144b6770cb0bf5661fd082
sameerkindia <sameerkhanaa5@gmail.com>
2025-02-27 09:45:04 +0530

    signin page

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,3 +3,8 @@ export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A mod
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000/"
 
 export const LATEST_PRODUCT_LIMIT = Number(4)
+
+export const signInDefaultValues={
+  email: '',
+  password: ''
+}
