b13941a3c3dea70be3c3301b296dc5aced5de9a9
sameerkindia <sameerkhanaa5@gmail.com>
2025-03-03 20:11:57 +0530

    add to cart

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,10 +1,10 @@
-import { notFound } from 'next/navigation';
-import ProductPrice from '@/components/shared/product/product-price';
-import { Card, CardContent } from '@/components/ui/card';
-import { getProductBySlug } from '@/lib/actions/product.actions';
-import { Button } from '@/components/ui/button';
-import { Badge } from '@/components/ui/badge';
-import ProductImages from '@/components/shared/product/product-images';
+import { notFound } from "next/navigation";
+import ProductPrice from "@/components/shared/product/product-price";
+import { Card, CardContent } from "@/components/ui/card";
+import { getProductBySlug } from "@/lib/actions/product.actions";
+import { Badge } from "@/components/ui/badge";
+import ProductImages from "@/components/shared/product/product-images";
+import AddToCart from "@/components/shared/product/add-to-cart";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -19,31 +19,31 @@ const ProductDetailsPage = async (props: {
   return (
     <>
       <section>
-        <div className='grid grid-cols-1 md:grid-cols-5'>
+        <div className="grid grid-cols-1 md:grid-cols-5">
           {/* Images Column */}
-          <div className='col-span-2'>
+          <div className="col-span-2">
             <ProductImages images={product.images} />
           </div>
 
           {/* Details Column */}
-         <div className='col-span-2 p-5'>
-            <div className='flex flex-col gap-6'>
+          <div className="col-span-2 p-5">
+            <div className="flex flex-col gap-6">
               <p>
                 {product.brand} {product.category}
               </p>
-              <h1 className='h3-bold'>{product.name}</h1>
+              <h1 className="h3-bold">{product.name}</h1>
               <p>
                 {product.rating} of {product.numReviews} reviews
               </p>
 
-               <div className='flex flex-col gap-3 sm:flex-row sm:items-center'>
-                  <ProductPrice
-                    value={Number(product.price)}
-                    className='w-24 rounded-full bg-green-100 text-green-700 px-5 py-2'
-                  />
+              <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+                <ProductPrice
+                  value={Number(product.price)}
+                  className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
+                />
               </div>
             </div>
-            <div className='mt-10'>
+            <div className="mt-10">
               <p>Description:</p>
               <p>{product.description}</p>
             </div>
@@ -51,24 +51,33 @@ const ProductDetailsPage = async (props: {
           {/* Action Column */}
           <div>
             <Card>
-              <CardContent className='p-4'>
-                <div className='mb-2 flex justify-between'>
+              <CardContent className="p-4">
+                <div className="mb-2 flex justify-between">
                   <label>Price</label>
                   <div>
                     <ProductPrice value={Number(product.price)} />
                   </div>
                 </div>
-                <div className='mb-2 flex justify-between'>
+                <div className="mb-2 flex justify-between">
                   <label>Status</label>
                   {product.stock > 0 ? (
-                    <Badge variant='outline'>In stock</Badge>
+                    <Badge variant="outline">In stock</Badge>
                   ) : (
-                    <Badge variant='destructive'>Unavailable</Badge>
+                    <Badge variant="destructive">Unavailable</Badge>
                   )}
                 </div>
                 {product.stock > 0 && (
-                  <div className=' flex-center'>
-                    <Button className='w-full'>Add to cart</Button>
+                  <div className=" flex-center">
+                    <AddToCart
+                      item={{
+                        productId: product.id,
+                        name: product.name,
+                        slug: product.slug,
+                        price: Number(product.price),
+                        qty: 1,
+                        image: product.images![0],
+                      }}
+                    />
                   </div>
                 )}
               </CardContent>
--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,19 +1,117 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+import { z } from 'zod';
+import { auth } from '@/auth';
+import { formatError, round2 } from '../utils';
+import { cartItemSchema, insertCartSchema } from '../validators';
+import { prisma } from '@/db/prisma';
 import { CartItem } from '@/types';
+import { Prisma } from '@prisma/client';
+import { convertToPlainObject } from '../utils';
 
 
-// @ts-ignore
-// export async function addItemToCart(data: CartItem) {
-//   return {
-//     success: true,
-//     message: 'Item added to the cart',
-//   };
-// };
+// Calculate cart price based on items
+const calcPrice = (items: z.infer<typeof cartItemSchema>[]) => {
+  const itemsPrice = round2(
+      items.reduce((acc, item) => acc + Number(item.price) * item.qty, 0)
+    ),
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 10),
+    taxPrice = round2(0.15 * itemsPrice),
+    totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  };
+};
 
-export const addItemToCart = async (data: CartItem) => {
+// Add item to cart in database
+export const addItemToCart = async (data: z.infer<typeof cartItemSchema>) => {
+  try {
+    // Check for session cart cookie
+    const sessionCartId = (await cookies()).get('sessionCartId')?.value;
+    if (!sessionCartId) throw new Error('Cart Session not found');
+    // Get session and user ID
+    const session = await auth();
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+    // Get cart from database
+    const cart = await getMyCart();
+
+    // Parse and validate submitted item data
+    const item = cartItemSchema.parse(data);
+    // Find product in database
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+    if (!product) throw new Error('Product not found');
+
+    if (!cart) {
+      // Create new cart object
+      const newCart = insertCartSchema.parse({
+        userId: userId,
+        items: [item],
+        sessionCartId: sessionCartId,
+        ...calcPrice([item]),
+      });
+      // Add to database
+      await prisma.cart.create({
+        data: newCart,
+      });
+    
+      // Revalidate product page
+      revalidatePath(`/product/${product.slug}`);
+    
+      return {
+        success: true,
+        message: 'Item added to cart successfully',
+      };
+    }
+
+    // Testing
+    console.log({
+      'Session Cart ID': sessionCartId,
+      'User ID': userId,
+      'Item Requested': item,
+      'Product Found': product,
+      cart: cart,
+    });
+
     return {
       success: true,
-      message: 'Item added to the cart',
+      message: 'Testing Cart',
     };
-  };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+};
+
+//  Get user cart from database
+export async function getMyCart() {
+  // Check for session cart cookie
+  const sessionCartId = (await cookies()).get('sessionCartId')?.value;
+  if (!sessionCartId) return undefined;
+
+  // Get session and user ID
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  // Get user cart from database
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) return undefined;
+
+  // Convert Decimal values to strings for compatibility with AddToCart component
+  return convertToPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
+}
