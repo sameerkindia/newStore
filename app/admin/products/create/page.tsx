f215cdc4c95d7f1d9d11b80eadcc8c92cc180c7f
sameerkindia <sameerkhanaa5@gmail.com>
2025-03-16 11:21:53 +0530

    image upload

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,4 +1,5 @@
-import ProductForm from '@/components/admin/product-form';
+import CreateProductForm from '@/components/admin/create-product-form';
+import ProductForm from '@/components/admin/create-product-form';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -9,7 +10,7 @@ const CreateProductPage = () => {
   return (
     <>
       <h2 className='h2-bold'>Create Product</h2>
-      <div className='my-8'><ProductForm type='Create' /></div>
+      <div className='my-8'><CreateProductForm /></div>
     </>
   );
 };
