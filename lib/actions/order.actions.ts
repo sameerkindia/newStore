94d1130e9fd8771f17a23fa849c9f5d21c825858
sameerkindia <sameerkhanaa5@gmail.com>
2025-03-21 09:33:14 +0530

    stripe payment

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -186,7 +186,7 @@ export async function approvePayPalOrder(
 
 
 // Update Order to Paid in Database
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
