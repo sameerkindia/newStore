18b502e2840c55cda1b4035239a57cad02aa2b85
sameerkindia <sameerkhanaa5@gmail.com>
2025-03-30 12:20:31 +0530

    auth and login route

--- app/api/cart-session.ts
+++
@@ -1,5 +0,0 @@
-export default function handler(req:any, res:any) {
-  const sessionCartId = crypto.randomUUID();
-  res.setHeader("Set-Cookie", `sessionCartId=${sessionCartId}; Path=/;`);
-  res.status(200).json({ sessionCartId });
-}
